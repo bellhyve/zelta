1c76b4a5835d0be1cd1d490dc6ced81d64b25608
Daniel J. Bell <daniel@belltech.it>
2024-02-04 11:27:14 -0500

    fix shift bug in some Bourne versions, add synonym links back because people use them

--- install.sh
+++ install.sh
@@ -22,6 +22,7 @@ fi
 
 : ${ZELTA_CONF:="$ZELTA_ETC/zelta.conf"}
 : ${ZELTA_ENV:="$ZELTA_ETC/zelta.env"}
+ZELTA="$ZELTA_BIN/zelta"
 
 copy_file() {
 	if [ -z "$3" ]; then
@@ -36,20 +37,30 @@ copy_file() {
 	fi
 }
 
+link_to_zelta() {
+	if [ ! -e "$ZELTA_BIN/$1" ]; then
+		echo "symlinking: $ZELTA -> $1"
+		ln -s $ZELTA "$ZELTA_BIN/$1"
+	fi
+}
+
 
 mkdir -p "$ZELTA_BIN" "$ZELTA_SHARE" "$ZELTA_ETC"
-copy_file bin/zelta "$ZELTA_BIN/zelta"
+copy_file bin/zelta "$ZELTA"
 find share/zelta -type f | while read -r file; do
     copy_file "$file" "${ZELTA_SHARE}/$(basename "$file")"
 done
+link_to_zelta zmatch
+link_to_zelta zpull
+link_to_zelta zp
 
 # Environment and default overrides
-if [ ! -s $ZELTA_ENV ]; then
+if [ ! -s "$ZELTA_ENV" ]; then
 	copy_file zelta.env "$ZELTA_ENV"
 	[ -x /usr/bin/time ] || echo 'TIME_COMMAND="zelta time"' >> "$ZELTA_ENV"
 fi
 
 # Example zelta policy
-if [ ! -s $ZELTA_CONF ]; then
+if [ ! -s "$ZELTA_CONF" ]; then
 	copy_file zelta.conf "$ZELTA_CONF" "644"
 fi
