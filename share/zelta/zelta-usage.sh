3776cf7415c1218a6b15612b875b4b8cabe06f48
Daniel Bell <daniel@belltech.it>
2024-05-25 14:19:22 -0400

    better missing source error

--- share/zelta/zelta-usage.sh
+++ share/zelta/zelta-usage.sh
@@ -27,12 +27,21 @@ For further help on a command or topic, run: zelta help [<topic>]
 EOF
 }
 
+runman() {
+	SECTION=8
+	if [ ! -s "$ZELTA_DOC" ] ; then
+		man "$ZELTA_DOC/$1.$SECTION"
+	else
+		man $SECTION $1
+	fi
+}
+
 # Add "man" if available.
 case $1 in
 	usage|help) usage_zelta ;;
-	backup|sync|clone|replicate) man zelta-backup ;;
-	match) man zelta-match ;;
-	policy) man zelta-policy ;;
+	backup|sync|clone|replicate) runman zelta-backup ;;
+	match) runman zelta-match ;;
+	policy) runman zelta-policy ;;
 	*)	[ -n "$1" ] && echo unrecognized command \'$1\' >>/dev/null ;
 		usage_zelta ;;
 esac
