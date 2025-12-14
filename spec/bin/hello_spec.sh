b38e18f66b815a06277f98dca36cbb84174e6212
Richard Logwood <richard.logwood@gmail.com>
2025-12-14 02:04:04 -0500

    use scripts for initialization, clean up error handling - wip

--- spec/bin/hello_spec.sh
+++ spec/bin/hello_spec.sh
@@ -6,26 +6,8 @@
 Describe 'hello shellspec'
   Include spec/lib/hello.sh
   setup() {
-    %logger "-- BeforeAll setup"
-    %logger "-- creating test pools"
-    "${INITIALIZE_DIR}"/create_file_backed_zfs_test_pools.sh 2>/dev/null
-    CREATE_STATUS=$?
-
-    %logger "-- installing zelta"
-    "${INITIALIZE_DIR}"/install_local_zelta.sh
-    INSTALL_STATUS=$?
-
-    %logger "-- setting up snap tree"
-    "${INITIALIZE_DIR}"/setup_simple_snap_tree.sh
-    TREE_STATUS=$?
-
-    %logger "-- Create pool status:    {$CREATE_STATUS}"
-    %logger "-- Install Zelta status:  {$INSTALL_STATUS}"
-    %logger "-- Make snap tree status: {$TREE_STATUS}"
-
-    SETUP_STATUS=$((CREATE_STATUS || INSTALL_STATUS || TREE_STATUS))
-    %logger "-- returning SETUP_STATUS:{$SETUP_STATUS}"
-    return $SETUP_STATUS
+    %logger "--hello spec setup"
+    spec/initialize/initialize_testing_setup.sh
   }
 
   cleanup() {
--- spec/initialize/initialize_testing_setup.sh
+++ spec/initialize/initialize_testing_setup.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
 
+pwd
 
+echo "-- BeforeAll setup"
+
+echo "-- installing zelta"
+"${INITIALIZE_DIR}"/install_local_zelta.sh
+INSTALL_STATUS=$?
+if [ $INSTALL_STATUS -ne 0 ]; then
+  echo "** Error: zelta install failed"
+fi
+
+TREE_STATUS=1
+
+echo "-- creating test pools"
+if "${INITIALIZE_DIR}"/create_file_backed_zfs_test_pools.sh; then
+   echo "-- setting up snap tree"
+   "${INITIALIZE_DIR}"/setup_simple_snap_tree.sh
+   TREE_STATUS=$?
+else
+   echo "** Error: failed to setup zfs pool" >&2
+fi
+
+#CREATE_STATUS=$?
+
+
+#echo "-- Create pool status:    {$CREATE_STATUS}"
+echo "-- Install Zelta status:  {$INSTALL_STATUS}"
+echo "-- Make snap tree status: {$TREE_STATUS}"
+
+#SETUP_STATUS=$((CREATE_STATUS || INSTALL_STATUS || TREE_STATUS))
+SETUP_STATUS=$((INSTALL_STATUS || TREE_STATUS))
+echo "-- returning SETUP_STATUS:{$SETUP_STATUS}"
+
+if [ $SETUP_STATUS -ne 0 ]; then
+   echo "** Error: zfs pool and/or zelta install failed!" >&2
+fi
+
+return $SETUP_STATUS
 
 # setting up test pools requires sudo
 
@@ -9,7 +46,7 @@
 #. spec/initialize/create_file_backed_zfs_test_pools.sh
 #set -x
 #zfs --help
-. ${INITIALIZE_DIR}/create_file_backed_zfs_test_pools.sh
+#. ${INITIALIZE_DIR}/create_file_backed_zfs_test_pools.sh
 #set +x
 #true
 # install zelta in a tmp directory locally for testing
