c51c8773d2531e8026393b11bf764b3363cf3ad1
Richard Logwood <richard.logwood@gmail.com>
2025-12-15 02:49:57 -0500

    use env vars for zfs top level dataset names

--- spec/bin/001_zelta_test_setup_spec.sh
+++ spec/bin/001_zelta_test_setup_spec.sh
@@ -15,10 +15,10 @@ Describe 'confirm zfs setup'
     It "has good initial SRC_POOL:{$SRC_POOL} simple snap tree"
         When call zfs list -r "$SRC_POOL"
         The line 2 of output should match pattern "* /$SRC_POOL"
-        The line 3 of output should match pattern "* /$SRC_POOL/treetop"
-        The line 4 of output should match pattern "* /$SRC_POOL/treetop/one"
-        The line 5 of output should match pattern "* /$SRC_POOL/treetop/one/two"
-        The line 6 of output should match pattern "* /$SRC_POOL/treetop/one/two/three"
+        The line 3 of output should match pattern "* /$SRC_POOL/$TREETOP_DSN"
+        The line 4 of output should match pattern "* /$SRC_POOL/$TREETOP_DSN/one"
+        The line 5 of output should match pattern "* /$SRC_POOL/$TREETOP_DSN/one/two"
+        The line 6 of output should match pattern "* /$SRC_POOL/$TREETOP_DSN/one/two/three"
      End
 
      It "has good initial TGT_POOL:{$TGT_POOL} simple snap tree"
