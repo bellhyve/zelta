8bdea2771963fed6743d2904ba87d32b13558edf
Richard Logwood <richard.logwood@gmail.com>
2026-01-02 00:09:56 +0000

    update tmp area perms, generalize test dir

--- spec/bin/ssh_tests_setup/setup_remote_host_test_env.sh
+++ spec/bin/ssh_tests_setup/setup_remote_host_test_env.sh
@@ -3,13 +3,15 @@
 TEST_BRANCH=feature/zelta-test
 TEST_USER=dever
 REMOTE_HOST=fzfsdev
-#pool_name="tank"
+TEST_DIR=/tmp/zelta-dev
+
 ssh ${TEST_USER}@${REMOTE_HOST} << EOF
 set -x
-rm -fr /tmp/zelta-dev
-mkdir -p /tmp/zelta-dev
-git clone https://github.com/bellhyve/zelta.git /tmp/zelta-dev
-cd /tmp/zelta-dev
+rm -fr ${TEST_DIR}
+mkdir -p ${TEST_DIR}
+chmod 777 ${TEST_DIR}
+git clone https://github.com/bellhyve/zelta.git ${TEST_DIR}
+cd ${TEST_DIR}
 git checkout $TEST_BRANCH
 ls
 sudo spec/bin/ssh_tests_setup/setup_zfs_pools_on_remote.sh
