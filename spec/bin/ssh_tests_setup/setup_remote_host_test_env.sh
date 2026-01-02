7cb6c671f2e6868d8548f72c9a8acca471cd6add
Richard Logwood <richard.logwood@gmail.com>
2026-01-02 08:17:54 +0000

    improve user interaciton, installation removal requires sudo, wip

--- spec/bin/ssh_tests_setup/setup_remote_host_test_env.sh
+++ spec/bin/ssh_tests_setup/setup_remote_host_test_env.sh
@@ -3,9 +3,11 @@
 . spec/bin/all_tests_setup/common_test_env.sh
 
 # pull down zelta from github
+printf "\n\n*** Enter sudo password to remove remote git clone directory ${BACKUP_USER}@${REMOTE_TEST_HOST}:${ZELTA_GIT_CLONE_DIR}\n"
+ssh -t ${BACKUP_USER}@${REMOTE_TEST_HOST} sudo rm -fr ${ZELTA_GIT_CLONE_DIR}
+
 ssh ${BACKUP_USER}@${REMOTE_TEST_HOST} << EOF
 set -x
-rm -fr ${ZELTA_GIT_CLONE_DIR}
 mkdir -p ${ZELTA_GIT_CLONE_DIR}
 chmod 777 ${ZELTA_GIT_CLONE_DIR}
 git clone https://github.com/bellhyve/zelta.git ${ZELTA_GIT_CLONE_DIR}
@@ -21,9 +23,11 @@ onetime_setup=${ZELTA_GIT_CLONE_DIR}/spec/bin/one_time_setup/setup_sudoers.sh
 setup_pools=${ZELTA_GIT_CLONE_DIR}/spec/bin/ssh_tests_setup/setup_zfs_pools_on_remote.sh
 
 # update sudo
+printf "\n\n*** Enter sudo password to perform remote sudo setup:\n"
 ssh -t ${BACKUP_USER}@${REMOTE_TEST_HOST} sudo "${onetime_setup}"
 
 # create pools
+printf "\n\n*** Enter sudo password to perform remote pools setup:\n"
 ssh -t ${BACKUP_USER}@${REMOTE_TEST_HOST} sudo "${setup_pools}"
 
 
