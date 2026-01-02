d5d03b077a2cb60af82d978d70342c8cf9a6cc54
Richard Logwood <richard.logwood@gmail.com>
2026-01-02 08:14:56 +0000

    define remotes without colon, wip running ssh tests

--- test/run_ssh_standard_tests.sh
+++ test/run_ssh_standard_tests.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 set -e
+curdir=$(pwd)
+echo "current dir is {$curdir}"
 
-export SRC_SVR="dever@fzfsdev:"
-export TGT_SVR="dever@fzfsdev:"
+export SRC_SVR="dever@fzfsdev"
+export TGT_SVR="dever@fzfsdev"
 # TODO: sort out how to get this to work
-# with SSH
 
-echo "continue here"
-echo "continue here"
-echo "continue here"
-echo "continue here"
-echo "continue here"
-echo "continue here"
-echo "continue here"
+echo "Step 3/3: Running zelta tests..."
+#shellspec ./spec/bin/standard_test/standard_test_spec.sh
 
-. test/run_standard_tests.sh
-
+shellspec -f d --xtrace spec/bin/standard_test/standard_test_spec.sh:@1
+#shellspec -f d spec/bin/standard_test/standard_test_spec.sh:@1-2
+shellspec -f d --xtrace spec/bin/standard_test/standard_test_spec.sh:@2-1
+#spec/bin/standard_test/standard_test_spec.sh:@2-2
+#spec/bin/standard_test/standard_test_spec.sh:@3-1
