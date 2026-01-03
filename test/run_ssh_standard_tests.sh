3edbfb00ce04ad6bc0b81179f2b6e9f10b1a77b3
Richard Logwood <richard.logwood@gmail.com>
2026-01-03 01:28:43 +0000

    run tests individually while sorting out configuration

--- test/run_ssh_standard_tests.sh
+++ test/run_ssh_standard_tests.sh
@@ -8,10 +8,10 @@ export TGT_SVR="dever@fzfsdev"
 # TODO: sort out how to get this to work
 
 echo "Step 3/3: Running zelta tests..."
-#shellspec ./spec/bin/standard_test/standard_test_spec.sh
+OPTIONS="${OPTIONS:-}"
+#shellspec -f d $OPTIONS ./spec/bin/standard_test/standard_test_spec.sh
+shellspec -f d $OPTIONS spec/bin/standard_test/standard_test_spec.sh:@1
+shellspec -f d $OPTIONS spec/bin/standard_test/standard_test_spec.sh:@2
+shellspec -f d $OPTIONS spec/bin/standard_test/standard_test_spec.sh:@3
 
-shellspec -f d --xtrace spec/bin/standard_test/standard_test_spec.sh:@1
-#shellspec -f d spec/bin/standard_test/standard_test_spec.sh:@1-2
-shellspec -f d --xtrace spec/bin/standard_test/standard_test_spec.sh:@2-1
-#spec/bin/standard_test/standard_test_spec.sh:@2-2
-#spec/bin/standard_test/standard_test_spec.sh:@3-1
+
