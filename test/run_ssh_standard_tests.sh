cc3e37116a011d1945a0c23ba5b9c2a28c2c7bac
Richard Logwood <richard.logwood@gmail.com>
2026-01-04 05:13:42 +0000

    clean up --xtrace check logic and options var, use SHELLSPEC_TESTOPT for specifying --xtrace --shell bash

--- test/run_ssh_standard_tests.sh
+++ test/run_ssh_standard_tests.sh
@@ -9,11 +9,11 @@ export TGT_SVR="dever@fzfsdev"
 # NOTE: see remote_test_setup for remote tree setup
 
 echo "Running zelta tests..."
-OPTIONS="${OPTIONS:-}"
-shellspec -f d $OPTIONS ./spec/bin/standard_test/standard_test_spec.sh
-#shellspec -f d $OPTIONS spec/bin/standard_test/standard_test_spec.sh:@1
-#shellspec -f d $OPTIONS spec/bin/standard_test/standard_test_spec.sh:@2
-#shellspec -f d $OPTIONS spec/bin/standard_test/standard_test_spec.sh:@3
+SHELLSPEC_TESTOPT="${SHELLSPEC_TESTOPT:-}"
+shellspec -f d "$SHELLSPEC_TESTOPT" ./spec/bin/standard_test/standard_test_spec.sh
+#shellspec -f d "$SHELLSPEC_TESTOPT" spec/bin/standard_test/standard_test_spec.sh:@1
+#shellspec -f d "$SHELLSPEC_TESTOPT" spec/bin/standard_test/standard_test_spec.sh:@2
+#shellspec -f d "$SHELLSPEC_TESTOPT" spec/bin/standard_test/standard_test_spec.sh:@3
 
 echo ""
 echo "✓ Tests complete"
