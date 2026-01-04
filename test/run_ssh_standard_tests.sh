074ac7f341f6c55c55831a0a3ed6ac7900d2aa14
Richard Logwood <richard.logwood@gmail.com>
2026-01-04 05:19:36 +0000

    cannot quote SHELLSPEC_TESTOPT, clean up examples and comments shown for this option

--- test/run_ssh_standard_tests.sh
+++ test/run_ssh_standard_tests.sh
@@ -9,11 +9,18 @@ export TGT_SVR="dever@fzfsdev"
 # NOTE: see remote_test_setup for remote tree setup
 
 echo "Running zelta tests..."
-SHELLSPEC_TESTOPT="${SHELLSPEC_TESTOPT:-}"
-shellspec -f d "$SHELLSPEC_TESTOPT" ./spec/bin/standard_test/standard_test_spec.sh
-#shellspec -f d "$SHELLSPEC_TESTOPT" spec/bin/standard_test/standard_test_spec.sh:@1
-#shellspec -f d "$SHELLSPEC_TESTOPT" spec/bin/standard_test/standard_test_spec.sh:@2
-#shellspec -f d "$SHELLSPEC_TESTOPT" spec/bin/standard_test/standard_test_spec.sh:@3
+
+# shellspec options to include
+#SHELLSPEC_TESTOPT="${SHELLSPEC_TESTOPT:-}"
+
+# this options will show a trace with expectation evaluation
+#SHELLSPEC_TESTOPT="--xtrace --shell bash"
+
+unset SHELLSPEC_TESTOPT
+shellspec -f d $SHELLSPEC_TESTOPT ./spec/bin/standard_test/standard_test_spec.sh
+#shellspec -f d $SHELLSPEC_TESTOPT spec/bin/standard_test/standard_test_spec.sh:@1
+#shellspec -f d $SHELLSPEC_TESTOPT spec/bin/standard_test/standard_test_spec.sh:@2
+#shellspec -f d $SHELLSPEC_TESTOPT spec/bin/standard_test/standard_test_spec.sh:@3
 
 echo ""
 echo "✓ Tests complete"
