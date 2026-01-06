015a8b1d426340cd5d7f7588836db9c6f85e2cba
Richard Logwood <richard.logwood@gmail.com>
2026-01-06 18:26:25 +0000

    standardize tree/dataset/pool constants

--- spec/bin/standard_test/standard_test_env.sh
+++ spec/bin/standard_test/standard_test_env.sh
@@ -3,15 +3,4 @@
 . spec/bin/all_tests_setup/common_test_env.sh
 . spec/lib/common.sh
 
-export TREETOP_DSN='treetop'
-export BACKUPS_DSN='backups'
-
-# zelta version for pool names will include the remote
-export SOURCE="${ZELTA_SRC_POOL}/${TREETOP_DSN}"
-export TARGET="${ZELTA_TGT_POOL}/${BACKUPS_DSN}/${TREETOP_DSN}"
-
-# zfs versions for pool names do not include th eremote
-export SRC_TREE="$SRC_POOL/$TREETOP_DSN"
-export TGT_TREE="$TGT_POOL/$BACKUPS_DSN/$TREETOP_DSN"
-
 export ALL_DATASETS="one/two/three"
