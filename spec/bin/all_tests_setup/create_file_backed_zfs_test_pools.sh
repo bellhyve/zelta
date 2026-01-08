086869b929e9460c374fcce808d38600335edc00
Richard Logwood <richard.logwood@gmail.com>
2026-01-08 06:28:05 +0000

    add support for FreeBSD memory disk pools, clean-up and standardize

--- spec/bin/all_tests_setup/create_file_backed_zfs_test_pools.sh
+++ spec/bin/all_tests_setup/create_file_backed_zfs_test_pools.sh
@@ -2,6 +2,71 @@
 
 . spec/lib/common.sh
 
+pool_image_file() {
+    pool_name=$1
+    echo "${ZELTA_ZFS_STORE_TEST_DIR}/${pool_name}.img" # return value is output to stdout
+}
+
+
+md_unit_file() {
+    pool_name=$1
+    echo "${ZELTA_ZFS_STORE_TEST_DIR}/${pool_name}.md" # return value is output to stdout
+}
+
+
+create_freebsd_mem_disk_pool() {
+    pool_name=$1
+    img_file=$(pool_image_file $pool_name)
+    create_file_img $img_file
+
+    # Attach as memory disk
+    md_unit=$(mdconfig -a -t vnode -f "$img_file")
+    echo "Created $md_unit for $pool_name"
+
+    # Create pool on the device
+    exec_cmd sudo zpool create "$pool_name" "/dev/$md_unit"
+
+    # Store md unit for cleanup
+    #echo "$md_unit" > "/tmp/${pool_name}.md"
+    md_file=$(md_unit_file "$pool_name")
+    echo "$md_unit" > "$md_file"
+}
+
+## Destroy image-backed pool on FreeBSD
+destroy_freebsd_mem_disk_pool() {
+    pool_name=$1
+    img_file=$(pool_image_file "$pool_name")
+
+    #img_file="/tmp/${pool_name}.img"
+    #img_file=$img_file
+    #md_file="/tmp/${pool_name}.md"
+    md_file=$(md_unit_file "$pool_name")
+
+#    destroy_pool_if_exists "$pool_name"
+#    # Destroy pool
+#    if exec_cmd sudo zpool list "$pool_name" >/dev/null 2>&1; then
+#        exec_cmd sudo zpool export "$pool_name" || zpool destroy -f "$pool_name"
+#    fi
+
+    # Detach md device
+    if [ -f "$md_file" ]; then
+        md_unit=$(cat "$md_file")
+        mdconfig -d -u "${md_unit#md}"
+        rm "$md_file"
+    fi
+
+    # Remove image file
+    rm -f "$img_file"
+}
+
+create_freebsd_test_pool() {
+    pool_name=$1
+    destroy_freebsd_mem_disk_pool $pool_name
+    create_freebsd_mem_disk_pool $pool_name
+}
+
+
+
 check_pool_exists() {
     pool_name="$1"
     if [ -z "$pool_name" ]; then
@@ -11,6 +76,19 @@ check_pool_exists() {
     exec_cmd sudo zpool list "$pool_name" >/dev/null 2>&1
 }
 
+
+
+destroy_pool() {
+    echo "Destroying pool '$pool_name'..."
+    exec_cmd sudo zpool export -f "$pool_name" && exec_cmd sudo zpool destroy -f "$pool_name"
+
+#    if ! exec_cmd sudo zpool destroy -f "$pool_name"; then
+#        echo "Destroy for pool '$pool_name' failed, trying export then destroy"
+#        # Export is only needed when the pool is busy/imported but destroy can't complete
+#        exec_cmd sudo zpool export -f "$pool_name" && exec_cmd sudo zpool destroy -f "$pool_name"
+#    fi
+}
+
 ## 1. Export (destroy) the pool
 #zfs destroy -r poolname  # destroys all datasets (optional, if you want to be thorough)
 #zpool destroy poolname   # destroys the pool itself
@@ -18,16 +96,14 @@ check_pool_exists() {
 ## 2. Detach the loop device
 #sudo losetup -d /dev/loop0  # replace loop0 with your actual loop device
 
+
+
+
+
 destroy_pool_if_exists() {
     pool_name="$1"
     if check_pool_exists "$pool_name"; then
-        echo "Destroying pool '$pool_name'..."
-        exec_cmd sudo zpool list "$pool_name"
-        if ! exec_cmd sudo zpool destroy -f "$pool_name"; then
-            echo "Destroy for pool '$pool_name' failed, trying export then destroy"
-            # Export is only needed when the pool is busy/imported but destroy can't complete
-            exec_cmd sudo zpool export -f "$pool_name" && exec_cmd sudo zpool destroy -f "$pool_name"
-        fi
+         destroy_pool "$pool_name"
     else
         echo "Pool '$pool_name' does not exist, no need to destroy"
     fi
@@ -60,45 +136,47 @@ rm_img_and_its_loop_devices() {
 #    exec_cmd sudo rm -f "$img"
 #}
 
-xxrm_img_and_its_loop_devices() {
-    img=$1
-    echo "removing loop devices associated with image file: {$img}"
-    #exec_cmd sudo losetup -j "$img" | cut -d: -f1 | xargs -r exec_cmd sudo losetup -d
-
-    loop_device=$(sudo losetup -j "$img" | cut -d: -f1)
-    echo "removing loop device {$loop_device}"
-    exec_cmd sudo losetup -d $loop_device
-    echo "removing image file: {$img}"
-    exec_cmd sudo rm -f "$img"
-}
+#xxrm_img_and_its_loop_devices() {
+#    img=$1
+#    echo "removing loop devices associated with image file: {$img}"
+#    #exec_cmd sudo losetup -j "$img" | cut -d: -f1 | xargs -r exec_cmd sudo losetup -d
+#
+#    loop_device=$(sudo losetup -j "$img" | cut -d: -f1)
+#    echo "removing loop device {$loop_device}"
+#    exec_cmd sudo losetup -d $loop_device
+#    echo "removing image file: {$img}"
+#    exec_cmd sudo rm -f "$img"
+#}
 
 create_file_img() {
-    pool_file_img=$1
+    img_file=$1
 
     # NOTE: important to remove the image file first so that all zfs metadata is removed.
     # truncate on an existing file will resize it and leave the metadata in place leading to crashes
-    rm -f "${pool_file_img}"
+    rm -f "${img_file}"
 
-    echo "Creating ${ZELTA_ZFS_TEST_POOL_SIZE}" "${pool_file_img}"
-    truncate -s "${ZELTA_ZFS_TEST_POOL_SIZE}" "${pool_file_img}"
+    echo "Creating ${ZELTA_ZFS_TEST_POOL_SIZE}" "${img_file}"
+    truncate -s "${ZELTA_ZFS_TEST_POOL_SIZE}" "${img_file}"
 
     echo "showing created file image:"
-    ls -lh "${pool_file_img}"
+    ls -lh "${img_file}"
 }
+# old name
+#create_pool_from_loop_device()
 
-create_pool_from_loop_device() {
+create_linux_loop_device_pool() {
     pool_name=$1
-    pool_file_img="${ZELTA_ZFS_STORE_TEST_DIR}/${pool_name}.img"
+    img_file=$(pool_image_file $pool_name)
 
-    rm_img_and_its_loop_devices "$pool_file_img"
+    rm_img_and_its_loop_devices "$img_file"
 
-    create_file_img "$pool_file_img"
+    create_file_img "$img_file"
 
-    echo "create loop device for file image: {$pool_file_img}"
-    exec_cmd sudo losetup -f "$pool_file_img"
+    echo "create loop device for file image: {$img_file}"
+    exec_cmd sudo losetup -f "$img_file"
 
-    loop_device=$(losetup --list --noheadings --output NAME --associated "$pool_file_img")
-    echo "created loop_device:{$loop_device} for image file:{$pool_file_img}"
+    loop_device=$(losetup --list --noheadings --output NAME --associated "$img_file")
+    echo "created loop_device:{$loop_device} for image file:{$img_file}"
 
     echo "create pool {$pool_name} for loop device {$loop_device}"
     exec_cmd sudo zpool create -f -m "/${pool_name}" "${pool_name}" "${loop_device}"
@@ -107,13 +185,12 @@ create_pool_from_loop_device() {
 
 create_pool_from_image_file() {
     pool_name=$1
-    pool_file_img="${ZELTA_ZFS_STORE_TEST_DIR}/${pool_name}.img"
+    img_file=$(pool_image_file $pool_name)
 
-    create_file_img "$pool_file_img"
-    echo "Creating zfs pool {$pool_name} from image file {$pool_file_img}"
-    exec_cmd sudo zpool create -f -m "/${pool_name}" "${pool_name}" "${pool_file_img}"
+    create_file_img "$img_file"
+    echo "Creating zfs pool {$pool_name} from image file {$img_file}"
+    exec_cmd sudo zpool create -f -m "/${pool_name}" "${pool_name}" "${img_file}"
 }
-
 
 create_test_pool() {
     pool_name="$1"
@@ -122,8 +199,10 @@ create_test_pool() {
         return 1
     fi
 
-    if [ "$POOL_TYPE" = "$LOOP_DEV_POOL" ]; then
-        create_pool_from_loop_device "$pool_name"
+    if [ "$POOL_TYPE" = "$MEM_DISK_POOL" ]; then
+        create_freebsd_test_pool $pool_name
+    elif [ "$POOL_TYPE" = "$LOOP_DEV_POOL" ]; then
+        create_linux_loop_device_pool "$pool_name"
     elif [ "$POOL_TYPE" = "$FILE_IMG_POOL" ]; then
         create_pool_from_image_file $pool_name
     else
