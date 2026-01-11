8943785bf4e678797a2122f9a171ddf0fa66f5cd
Daniel J. Bell <daniel@belltower.it>
2026-01-11 00:32:34 -0500

    simple prune proof of concept

--- test/prune.sh
+++ test/prune.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
 DSNAME="$1"
 KEEP_MIN=10
 KEEP_DAYS=90
 [ -z "$1" ] && exit
-KEEP_MIN=$(($(zfs list -Hroname -d1 "$DSNAME"|wc -l)*KEEP_MIN))
-KEEP_DAYS="$(($(date +%s)-60*60*24*KEEP_DAYS))"
+KEEP_DAYS=$(($(date +%s)-60*60*24*KEEP_DAYS))
 
-zfs list -Hprtsnap -oname,creation -screation "$DSNAME" | awk -v min=$KEEP_MIN -v days=$KEEP_DAYS '
+zfs list -Hprtsnap -oname,creation -d1 -screation "$DSNAME" | awk -v min=$KEEP_MIN -v days=$KEEP_DAYS '
 {
 	if ($2<days) {
 		x[++y]=$1
-		if (x[y-min])
-			print x[y-min]
+		if (x[y-min]) z = x[y-min]
+	}
+}
+END {
+	if (z) {
+		sub(/@/, "@%", z)
+		print "zfs destroy -vrn \"" z "\""
 	}
 }'
