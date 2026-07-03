ec228a29a28e2a92f816597cce81ba5327ec92f2
Richard Logwood <richard.logwood@gmail.com>
2026-07-03 03:41:28 -0400

    fix debug processing

--- test/runners/test_generation/lib/ruby/env_substitutor.rb
+++ test/runners/test_generation/lib/ruby/env_substitutor.rb
@@ -15,19 +15,18 @@ class EnvSubstitutor
   end
 
   def debug_mode?
-    %w[yes 1 true].include?(ENV["ZELTA_TESTGEN_DEBUG"].to_s.downcase)
+    #%w[yes 1 true].include?(ENV["ZELTA_TESTGEN_DEBUG"].to_s.downcase)
+    true
   end
 
   def load_env_from_script(relative_path)
     script = File.expand_path(relative_path, __dir__)
     # Source the script in a subshell, then dump the env
-    if debug_mode?
-      env_output = `bash -c 'source #{script} && env' | grep SANDBOX`
-      env_output.each_line do |line|
-        key, value = line.strip.split('=', 2)
-        puts "Key: #{key}, Value: #{value}"
-        ENV[key] = value if key && value
-      end
+    env_output = `bash -c 'source #{script} && env' | grep SANDBOX`
+    env_output.each_line do |line|
+      key, value = line.strip.split('=', 2)
+      puts "Key: #{key}, Value: #{value}" if debug_mode?
+      ENV[key] = value if key && value
     end
   end
 
