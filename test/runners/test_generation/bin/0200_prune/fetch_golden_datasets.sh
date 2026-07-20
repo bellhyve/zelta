83d384dadc6d9ef7bd3d096c06af0eaa34792c48
Richard Logwood <richard.logwood@gmail.com>
2026-07-20 02:18:47 -0400

    use test-fixtures/v2

--- test/runners/test_generation/bin/0200_prune/fetch_golden_datasets.sh
+++ test/runners/test_generation/bin/0200_prune/fetch_golden_datasets.sh
@@ -6,5 +6,5 @@ fi
 : "${SANDBOX_ZELTA_GOLD_PRUNE_SCEN_DIR:=${SHELLSPEC_HELPERDIR}/golden/0200_prune}"
 
 mkdir -p "$SANDBOX_ZELTA_GOLD_PRUNE_SCEN_DIR"
-gh release download test-fixtures/v1 --pattern '*.img.gz' --dir "$SANDBOX_ZELTA_GOLD_PRUNE_SCEN_DIR"
+gh release download test-fixtures/v2 --pattern '*.img.gz' --dir "$SANDBOX_ZELTA_GOLD_PRUNE_SCEN_DIR"
 gunzip "$SANDBOX_ZELTA_GOLD_PRUNE_SCEN_DIR"/*.gz
